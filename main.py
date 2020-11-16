87cc2ecfce14e58f1ca3958305368e439d695e29
SatisfyingCommitChart <74567162+SatisfyingCommitChart@users.noreply.github.com>
2020-11-16 13:31:52 -0700

    Update main.py
    OOPS

--- main.py
+++ main.py
@@ -17,6 +17,6 @@ def commitFiles():
     cp = cmd.run("git push -u origin master -f", check=True, shell=True)
     
 scheduler = BlockingScheduler()
-scheduler.add_job(dailyCommits, 'interval', seconds = 1)
+scheduler.add_job(dailyCommits, 'interval', minutes = 30)
 # scheduler.add_job(commitFiles, 'interval', seconds = 15)
 scheduler.start()
